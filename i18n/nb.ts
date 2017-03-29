c5bc42ae0e4a3c7e1f26a7a1f269da8da268e4ce
havatv <havard.tveite@nmbu.no>
2017-03-29 17:15:49 +0200

    User interface updates (translations)

--- i18n/nb.ts
+++ i18n/nb.ts
@@ -1,17 +1,16 @@
 <?xml version="1.0" encoding="utf-8"?>
-<!DOCTYPE TS>
-<TS version="2.0" language="nb" sourcelanguage="en">
+<!DOCTYPE TS><TS version="2.0" language="nb" sourcelanguage="en">
 <context>
     <name>ThinGreyscale</name>
     <message>
-        <location filename="ThinGreyscale.py" line="181"/>
+        <location filename="ThinGreyscale.py" line="69"/>
         <source>&amp;Thin greyscale image to skeleton</source>
-        <translation type="unfinished">&amp;Tynn gråtonebilde til skjelett</translation>
+        <translation>&amp;Tynn gråtonebilde til skjelett</translation>
     </message>
     <message>
-        <location filename="ThinGreyscale.py" line="71"/>
+        <location filename="ThinGreyscale.py" line="70"/>
         <source>&amp;ThinGreyscale</source>
-        <translation type="unfinished">&amp;TynnGråtone</translation>
+        <translation>&amp;TynnGråtone</translation>
     </message>
 </context>
 <context>
@@ -19,7 +18,7 @@
     <message>
         <location filename="ThinGreyscale_dialog.py" line="60"/>
         <source>ThinGreyscale</source>
-        <translation type="unfinished">TynnGråtone</translation>
+        <translation>TynnGråtone</translation>
     </message>
     <message>
         <location filename="ThinGreyscale_dialog.py" line="61"/>
@@ -29,12 +28,12 @@
     <message>
         <location filename="ThinGreyscale_dialog.py" line="62"/>
         <source>Cancel</source>
-        <translation type="unfinished">Avbryt</translation>
+        <translation>Avbryt</translation>
     </message>
     <message>
         <location filename="ThinGreyscale_dialog.py" line="63"/>
         <source>Close</source>
-        <translation type="unfinished">Lukk</translation>
+        <translation>Lukk</translation>
     </message>
     <message>
         <location filename="ThinGreyscale_dialog.py" line="64"/>
@@ -42,54 +41,54 @@
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="ThinGreyscale_dialog.py" line="147"/>
+        <location filename="ThinGreyscale_dialog.py" line="148"/>
         <source>No input layer defined</source>
-        <translation type="unfinished">Innlag mangler</translation>
+        <translation>Innlag mangler</translation>
     </message>
     <message>
-        <location filename="ThinGreyscale_dialog.py" line="170"/>
+        <location filename="ThinGreyscale_dialog.py" line="171"/>
         <source>Skeletonising</source>
-        <translation type="unfinished">Skjelettifisering</translation>
+        <translation>Skjelettifisering</translation>
     </message>
     <message>
-        <location filename="ThinGreyscale_dialog.py" line="295"/>
+        <location filename="ThinGreyscale_dialog.py" line="297"/>
         <source>ThinGreyscale finished</source>
-        <translation type="unfinished">TynnGråtone fullført</translation>
-    </message>
-    <message>
-        <location filename="ThinGreyscale_dialog.py" line="300"/>
-        <source>Aborted</source>
-        <translation type="unfinished">Avbrutt</translation>
+        <translation>TynnGråtone fullført</translation>
     </message>
     <message>
         <location filename="ThinGreyscale_dialog.py" line="302"/>
+        <source>Aborted</source>
+        <translation>Avbrutt</translation>
+    </message>
+    <message>
+        <location filename="ThinGreyscale_dialog.py" line="304"/>
         <source>No skeleton created</source>
-        <translation type="unfinished">Skjelettet kunne ikke lages</translation>
+        <translation>Skjelettet kunne ikke lages</translation>
     </message>
     <message>
-        <location filename="ThinGreyscale_dialog.py" line="319"/>
+        <location filename="ThinGreyscale_dialog.py" line="322"/>
         <source>Worker</source>
-        <translation type="unfinished">Arbeider</translation>
+        <translation>Arbeider</translation>
     </message>
     <message>
-        <location filename="ThinGreyscale_dialog.py" line="781"/>
+        <location filename="ThinGreyscale_dialog.py" line="784"/>
         <source>Killing worker</source>
-        <translation type="unfinished">Avbryter arbeider</translation>
+        <translation>Avbryter arbeider</translation>
     </message>
     <message>
-        <location filename="ThinGreyscale_dialog.py" line="787"/>
+        <location filename="ThinGreyscale_dialog.py" line="790"/>
         <source>Error</source>
-        <translation type="unfinished">Feil</translation>
+        <translation>Feil</translation>
     </message>
     <message>
-        <location filename="ThinGreyscale_dialog.py" line="795"/>
+        <location filename="ThinGreyscale_dialog.py" line="798"/>
         <source>Warning</source>
-        <translation type="unfinished">Advarsel</translation>
+        <translation>Advarsel</translation>
     </message>
     <message>
-        <location filename="ThinGreyscale_dialog.py" line="803"/>
+        <location filename="ThinGreyscale_dialog.py" line="806"/>
         <source>Info</source>
-        <translation type="unfinished">Info</translation>
+        <translation>Info</translation>
     </message>
 </context>
 <context>
@@ -97,17 +96,17 @@
     <message>
         <location filename="ThinGreyscale_dialog_base.ui" line="14"/>
         <source>Thin greyscale image to skeleton</source>
-        <translation type="unfinished">Tynn gråtonebilde til skjelett</translation>
+        <translation>Tynn gråtonebilde til skjelett</translation>
     </message>
     <message>
         <location filename="ThinGreyscale_dialog_base.ui" line="20"/>
         <source>Input raster layer</source>
-        <translation type="unfinished">Innrasterlag</translation>
+        <translation>Innrasterlag</translation>
     </message>
     <message>
         <location filename="ThinGreyscale_dialog_base.ui" line="44"/>
         <source>Band:</source>
-        <translation type="unfinished">Bånd:</translation>
+        <translation>Bånd:</translation>
     </message>
     <message>
         <location filename="ThinGreyscale_dialog_base.ui" line="54"/>
@@ -115,104 +114,154 @@
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="ThinGreyscale_dialog_base.ui" line="385"/>
+        <location filename="ThinGreyscale_dialog_base.ui" line="404"/>
         <source>0</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="ThinGreyscale_dialog_base.ui" line="87"/>
+        <location filename="ThinGreyscale_dialog_base.ui" line="90"/>
         <source>Max:</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="ThinGreyscale_dialog_base.ui" line="405"/>
+        <location filename="ThinGreyscale_dialog_base.ui" line="424"/>
         <source>255</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="ThinGreyscale_dialog_base.ui" line="118"/>
+        <location filename="ThinGreyscale_dialog_base.ui" line="124"/>
         <source>Levels</source>
-        <translation type="unfinished">Nivåer</translation>
+        <translation>Nivåer</translation>
     </message>
     <message>
-        <location filename="ThinGreyscale_dialog_base.ui" line="126"/>
+        <location filename="ThinGreyscale_dialog_base.ui" line="132"/>
         <source>Min. value:</source>
-        <translation type="unfinished">Min.-verdi</translation>
+        <translation>Min.-verdi</translation>
     </message>
     <message>
-        <location filename="ThinGreyscale_dialog_base.ui" line="133"/>
+        <location filename="ThinGreyscale_dialog_base.ui" line="139"/>
         <source>Values lower than this will be ignored</source>
-        <translation type="unfinished">Lavere verdier enn denne blir ignorert</translation>
+        <translation>Lavere verdier enn denne blir ignorert</translation>
     </message>
     <message>
-        <location filename="ThinGreyscale_dialog_base.ui" line="159"/>
+        <location filename="ThinGreyscale_dialog_base.ui" line="165"/>
         <source>Max. value</source>
-        <translation type="unfinished">Maksverdi</translation>
+        <translation>Maksverdi</translation>
     </message>
     <message>
-        <location filename="ThinGreyscale_dialog_base.ui" line="166"/>
+        <location filename="ThinGreyscale_dialog_base.ui" line="172"/>
         <source>Values higher than this will be ignored (make sure you know what you are doing before you change this value)</source>
-        <translation type="unfinished">Verdier som er høydere enn denne blir ignorert (du bør ha en god grunn for å endre på denne)</translation>
+        <translation>Verdier som er høydere enn denne blir ignorert (du bør ha en god grunn for å endre på denne)</translation>
     </message>
     <message>
-        <location filename="ThinGreyscale_dialog_base.ui" line="186"/>
-        <source>Number of levels:</source>
-        <translation type="unfinished">Antall nivå:</translation>
-    </message>
-    <message>
-        <location filename="ThinGreyscale_dialog_base.ui" line="193"/>
+        <location filename="ThinGreyscale_dialog_base.ui" line="199"/>
         <source>The number of levels to suggest</source>
-        <translation type="unfinished">Antall invå som foreslås</translation>
+        <translation>Antall invå som foreslås</translation>
     </message>
     <message>
-        <location filename="ThinGreyscale_dialog_base.ui" line="212"/>
+        <location filename="ThinGreyscale_dialog_base.ui" line="218"/>
         <source>Suggest levels</source>
-        <translation type="unfinished">Foreslå nivå</translation>
+        <translation>Foreslå nivå</translation>
     </message>
     <message>
-        <location filename="ThinGreyscale_dialog_base.ui" line="268"/>
+        <location filename="ThinGreyscale_dialog_base.ui" line="277"/>
         <source>Add level</source>
-        <translation type="unfinished">Legg til nivå</translation>
+        <translation>Legg til nivå</translation>
     </message>
     <message>
-        <location filename="ThinGreyscale_dialog_base.ui" line="284"/>
+        <location filename="ThinGreyscale_dialog_base.ui" line="293"/>
         <source>Delete</source>
-        <translation type="unfinished">Slett</translation>
+        <translation>Slett</translation>
     </message>
     <message>
-        <location filename="ThinGreyscale_dialog_base.ui" line="322"/>
+        <location filename="ThinGreyscale_dialog_base.ui" line="331"/>
         <source>Histogram</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="ThinGreyscale_dialog_base.ui" line="342"/>
+        <location filename="ThinGreyscale_dialog_base.ui" line="351"/>
         <source>Calculate histogram</source>
-        <translation type="unfinished">Beregn histogram</translation>
+        <translation>Beregn histogram</translation>
     </message>
     <message>
-        <location filename="ThinGreyscale_dialog_base.ui" line="476"/>
+        <location filename="ThinGreyscale_dialog_base.ui" line="498"/>
         <source>NAN%:</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="ThinGreyscale_dialog_base.ui" line="511"/>
+        <location filename="ThinGreyscale_dialog_base.ui" line="543"/>
         <source>Output raster (thinned)</source>
-        <translation type="unfinished">Utraster (tynnet)</translation>
+        <translation>Utraster (tynnet)</translation>
     </message>
     <message>
-        <location filename="ThinGreyscale_dialog_base.ui" line="526"/>
+        <location filename="ThinGreyscale_dialog_base.ui" line="558"/>
         <source>Browse</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="ThinGreyscale_dialog_base.ui" line="535"/>
-        <source>Use the real level values as pixel values in the output raster, instead of the index of the level</source>
-        <translation type="unfinished">Benytt de originale nivåverdiene som pixelverdier i utrasteret i stedet for indeksen til nivået</translation>
+        <location filename="ThinGreyscale_dialog_base.ui" line="570"/>
+        <source>Use level values as output pixel values</source>
+        <translation>Benytt nivåverdier som ut-pixelverdier</translation>
     </message>
     <message>
-        <location filename="ThinGreyscale_dialog_base.ui" line="538"/>
-        <source>Use level values as output pixel values</source>
-        <translation type="unfinished">Benytt nivåverdier som ut-pixelverdier</translation>
+        <location filename="ThinGreyscale_dialog_base.ui" line="70"/>
+        <source>The minimum pixel value</source>
+        <translation>Minste pixelverdi</translation>
+    </message>
+    <message>
+        <location filename="ThinGreyscale_dialog_base.ui" line="106"/>
+        <source>The maximum pixel value</source>
+        <translation>Største pixelverdi</translation>
+    </message>
+    <message>
+        <location filename="ThinGreyscale_dialog_base.ui" line="192"/>
+        <source>Number of output levels:</source>
+        <translation>Antall utlag</translation>
+    </message>
+    <message>
+        <location filename="ThinGreyscale_dialog_base.ui" line="395"/>
+        <source>The red lines show the upper and lower cut-off values.</source>
+        <translation>De røde linjene viser øvre og nedre avkuttingsgrense.</translation>
+    </message>
+    <message>
+        <location filename="ThinGreyscale_dialog_base.ui" line="462"/>
+        <source>Total number of pixels considered</source>
+        <translation>Totalt antal pixler som vurderes</translation>
+    </message>
+    <message>
+        <location filename="ThinGreyscale_dialog_base.ui" line="514"/>
+        <source>NAN percentage for the original image</source>
+        <translation>NAN-andel for originalbildet</translation>
+    </message>
+    <message>
+        <location filename="ThinGreyscale_dialog_base.ui" line="567"/>
+        <source>Use the real level values as pixel values in the output raster, instead of the integer index of the level</source>
+        <translation>Bruk nivåverdiene i utrasteret i stedet for heltallsindeksene til nivåene</translation>
+    </message>
+    <message>
+        <location filename="ThinGreyscale_dialog_base.ui" line="598"/>
+        <source>Levels:</source>
+        <translation>Nivåer:</translation>
+    </message>
+    <message>
+        <location filename="ThinGreyscale_dialog_base.ui" line="611"/>
+        <source>Progresses as the levels are processed</source>
+        <translation>Utvikler seg ettersom nivåene prosesseres</translation>
+    </message>
+    <message>
+        <location filename="ThinGreyscale_dialog_base.ui" line="628"/>
+        <source>Progression for step within a level</source>
+        <translation>Utvikling for steg innen et nivå</translation>
+    </message>
+    <message>
+        <location filename="ThinGreyscale_dialog_base.ui" line="254"/>
+        <source>Level boundaries.&lt;br&gt;The topmost number is the minimum. The number at the bottom is the maximum value.</source>
+        <translation>Nivågrenser.&lt;br&gt;Det øverste tallet er minimumverdi. The nederste tallet er maksimumverdi.</translation>
+    </message>
+    <message>
+        <location filename="ThinGreyscale_dialog_base.ui" line="621"/>
+        <source>Iterations:</source>
+        <translation>Iterasjoner:</translation>
     </message>
 </context>
 </TS>
